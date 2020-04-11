85d12cabf45d29db71e6283b3bd0855e9da56b88
Victor Johnson <vicjohnson1213@gmail.com>
2020-04-11 18:22:03 -0500

    Generating a tree structure

--- ngdeps.js
+++ ngdeps.js
@@ -3,9 +3,33 @@ const path = require('path');
 
 const testDir = 'test';
 
-getTsFiles(path.join(__dirname, testDir))
-    .then(files => Promise.all(files.map(f => getImportsExports(path.join(__dirname, testDir, f)))))
-    .then(importsExports => console.log(importsExports));
+walk(path.join(__dirname, testDir))
+    .then(res => {
+        console.log(JSON.stringify(res, ' ', 4));
+    });
+
+async function walk(dir, currentDir = '') {
+    let files = await fs.readdir(dir);
+    files = await Promise.all(files.map(async file => {
+        const filepath = path.join(dir, file);
+        const stats = await fs.stat(filepath);
+
+        if (stats.isDirectory()) {
+            const kiddos = await walk(filepath, path.join(currentDir, file));
+            return { name: file, children: kiddos };
+        } else if (stats.isFile()) {
+            const importsExports = await getImportsExports(filepath);
+            return {
+                name: file,
+                path: path.join(currentDir, file),
+                imports: importsExports.imports,
+                exports: importsExports.exports
+            };
+        }
+    }));
+    
+    return files;
+}
 
 async function getImportsExports(filepath) {
     return await fs.readFile(filepath, 'utf8')
@@ -30,21 +54,4 @@ async function getImportsExports(filepath) {
 
             return { exports: exports, imports: imports };
         });
-}
-
-async function getTsFiles(dir, currentDir = '') {
-    let files = await fs.readdir(dir);
-    files = await Promise.all(files.map(async file => {
-        const filePath = path.join(dir, file);
-        const stats = await fs.stat(filePath);
-
-        if (stats.isDirectory()) {
-            return getTsFiles(filePath, path.join(currentDir, file));
-        } else if (stats.isFile()) {
-            return path.join(currentDir, file);
-        }
-    }));
-    
-    return files.reduce((all, folderContents) => all.concat(folderContents), [])
-        .filter(file => file.split('.').pop() === 'ts');
 }
